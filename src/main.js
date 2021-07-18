2e29c927c69ad53b479bdbf9aeeb635ceab0eff2
natsu08-n <merrysheep18@gmail.com>
2021-07-18 23:33:41 +0900

    firebaseの詳細設定

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import firebase from 'firebase';
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDeRev794IgesWbl0uAH77Bs4ZatA5Zfas",
+  authDomain: "vue-diary-app-c7999.firebaseapp.com",
+  projectId: "vue-diary-app-c7999",
+  storageBucket: "vue-diary-app-c7999.appspot.com",
+  messagingSenderId: "1082200385567",
+  appId: "1:1082200385567:web:1fe23c1f38d26b79478d2a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
